f59a7415244440051e24bf2bf88746290fa85313
Emir Jasarevic <emir2000@hotmail.se>
2024-06-17 16:48:47 +0200

    Created the project

---
+++ src/react/components/Main.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+export default function Main(){
+    const ExtractLeads = async () => {
+    };
+
+    return (
+        <main>
+            <h1>Lead Extractor</h1>
+            <button onClick={ExtractLeads}>Extract</button>
+        </main>
+    );
+}
+
+
+
